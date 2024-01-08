22083013a18be94e7b763bec37daeeda244a5cd5
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-01-08 17:03:39 -0600

    comments

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,7 @@ from .models import Post, Author, Tag
 # Register your models here.
 
 # config how the admin site looks
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(admin.ModelAdmin): # use ModelAdmin to make changes
     list_filter = ('author', 'tags', 'date',) # add tuples to create filters for views
     list_display = ('title', 'date', 'author',) # this to add columns
     prepopulated_fields = {'slug': ('title',)} # to prepopulate the slug field
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render, get_object_or_404
+
 from .models import Post
 
 
 # Create your views here.
 
+# include a start page w only the 3 latest posts
 def starting_page(request):
-    latest_posts = Post.objects.all().order_by('-date')[:3] # order by desc date; [-1] does not work in this django case
+    latest_posts = Post.objects.all().order_by('-date')[:3] # order by desc date '-date'; [-1] does not work in this django case for lists; django recognizes this as a SQL query, so performance is optimized and doesn't search whole list, instead it filters by WHERE clause to get 3 latest posts
     return render(request, 'blog/index.html', {
         'posts': latest_posts
     })
 
 
+# include an all posts page that shows all posts
 def posts(request):
     all_posts = Post.objects.all().order_by('-date')
     return render(request, 'blog/all-posts.html', {
@@ -18,6 +21,7 @@ def posts(request):
     })
 
 
+# include the post detail including its content
 def post_detail(request, slug):
     # identified_post = Post.objects.get(slug=slug)
     identified_post = get_object_or_404(Post, slug=slug) # get_404 gets an object, then with condition specified
